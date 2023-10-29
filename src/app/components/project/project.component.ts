97b88d74cb5c4e8e6416a763f57605a9f5143383
Nahuel Zalazar <zvnahuel63@gmail.com>
2023-10-29 19:47:51 -0400

    images

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -12,7 +12,7 @@ export class ProjectComponent {
       title: 'Template NestJS',
       description: 'Template para iniciar un proyecto con NestJS, TypeORM, Postgres, Swagger, Passport, JWT, Docker, Docker Compose, etc.',
       dir_project: 'https://github.com/nahuelXZV/NestJS-Template',
-      dir_image: 'assets/images/cheese-house.png',
+      dir_image: 'assets/projects/nest-template.png',
       habilities: [
         'NestJS',
         'TypeScript',
@@ -25,7 +25,7 @@ export class ProjectComponent {
       title: 'API Aplicacion web para eventos',
       description: 'API para una aplicación web para eventos, incluye gestión de usuarios, eventos, asistentes, IA para reconocer invitados en las fotos, etc.',
       dir_project: 'https://github.com/nahuelXZV/Back-Eventos',
-      dir_image: 'assets/images/cheese-house.png',
+      dir_image: 'assets/projects/eventos.png',
       habilities: [
         'NestJS',
         'TypeScript',
@@ -39,7 +39,7 @@ export class ProjectComponent {
       title: 'API Sistema de seguridad para estadios de fútbol mediante reconocimiento facial',
       description: 'Sistema de seguridad para estadios de fútbol mediante reconocimiento facial, incluye gestión de usuarios, cámaras, estadios, infractores, etc.',
       dir_project: 'https://github.com/nahuelXZV/Back-Seguridad',
-      dir_image: 'assets/images/cheese-house.png',
+      dir_image: 'assets/projects/seguridad.png',
       habilities: [
         'NestJS',
         'TypeScript',
@@ -52,7 +52,7 @@ export class ProjectComponent {
       title: 'Sistema de Gestión para una ferretería via email',
       description: 'Sistema de gestión para una ferretería mediante consultas via correo electrónico, incluye gestión de clientes, compra, venta, reportes, etc. ',
       dir_project: 'https://github.com/nahuelXZV/Galba-Email',
-      dir_image: 'assets/images/cheese-house.png',
+      dir_image: 'assets/defer.avif',
       habilities: [
         'Java',
         'Pop3',
@@ -64,7 +64,7 @@ export class ProjectComponent {
       title: 'Template NodeJS Microservicios',
       description: 'Template para iniciar un proyecto con NodeJS y Express basado en microservicios, incluye autenticación con JWT.',
       dir_project: 'https://github.com/nahuelXZV/NodeJs-MicroServicios',
-      dir_image: 'assets/images/cheese-house.png',
+      dir_image: 'assets/defer.avif',
       habilities: [
         'NodeJS',
         'Express',
@@ -76,7 +76,7 @@ export class ProjectComponent {
       title: 'Template NodeJS y Express',
       description: 'Template para iniciar un proyecto con NodeJS y Express, incluye autenticación con JWT.',
       dir_project: 'https://github.com/nahuelXZV/NodeJS-Express-Template',
-      dir_image: 'assets/images/cheese-house.png',
+      dir_image: 'assets/defer.avif',
       habilities: [
         'NodeJS',
         'Express',
@@ -87,7 +87,7 @@ export class ProjectComponent {
       title: 'Sistema de Gestión para una veterinaria',
       description: 'Sistema de gestión para una veterinaria, incluye gestión de clientes, compra, venta, mascotas, reportes, etc.',
       dir_project: 'https://github.com/nahuelXZV/Sistema-Veterinaria',
-      dir_image: 'assets/images/cheese-house.png',
+      dir_image: 'assets/defer.avif',
       habilities: [
         'Laravel',
         'Tailwind CSS',
